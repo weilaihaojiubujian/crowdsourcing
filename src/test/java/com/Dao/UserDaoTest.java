b29aa13fa49d17ab0a230fef0f71619f4bb9077e
汪宏斌 <35726025+weilaihaojiubujian@users.noreply.github.com>
2018-11-10 20:53:30 +0800

    mapper3

---
+++ src/test/java/com/Dao/UserDaoTest.java
@@ -0,0 +1,47 @@
+package com.Dao;
+
+import com.Entity.User;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import javax.annotation.Resource;
+
+/**
+ * @Author: wanghongbin
+ * @Description:
+ * @Date: Create in 12:24 2018/11/10
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+//告诉junit spring配置文件
+@ContextConfiguration("classpath:spring/spring-dao.xml")
+public class UserDaoTest {
+
+    @Resource
+    private UserDao userDao;
+
+    @Test
+    public void insertUser()
+    {
+        User u=new User();
+      String username="weilai";
+      String password="123456";
+      String phonenumber="13758149770";
+      String species="people";
+      u.setUsername(username);
+      u.setPassword(password);
+      u.setPhonenumber(phonenumber);
+      u.setSpecies(species);
+      int i=userDao.insertUser(u);
+        System.out.println(i);
+
+
+
+    }
+
+
+
+
+
+}
