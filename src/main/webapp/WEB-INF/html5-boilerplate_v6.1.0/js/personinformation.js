33d73e98c26e98bc12aa01028393ea2e7f0e3102
Jiaying Shan <43174910+sssssjy@users.noreply.github.com>
2018-11-27 18:57:08 +0800

    update

---
+++ src/main/webapp/WEB-INF/html5-boilerplate_v6.1.0/js/personinformation.js
@@ -0,0 +1,37 @@
+$(function () {
+	 $( "#datepicker" ).datepicker();
+	var $tableItems= $("[name='personMessage']").children();
+	// alert($tableMessage.eq(0).html());
+	var $table = $(".showInformation");
+	var $form = $(".changeInformation");
+	var $modifyMessageButton = $("button[name='modifyMessage']");
+	var $showMessageButton = $("button[name='showMessage']");
+	$table.hide().slideDown();
+	$form.hide();
+	$modifyMessageButton.on('click', function(event) {
+		event.preventDefault();
+		$table.slideUp();
+		$form.slideDown();
+	});
+	$showMessageButton.on('click', function(event) {
+		event.preventDefault();
+		$("#id").val($tableItems.eq(0).html());
+		$("#userName").val($tableItems.eq(1).html());
+		// $("#password").val($tableItems.eq(2).html());
+		$("#tel").val($tableItems.eq(3).html());
+		$("#name").val($tableItems.eq(4).html());
+		if ($tableItems.eq(5).html()=="女") {
+			$("#female").attr("checked",true);
+		}
+		else {
+			$("#male").attr("checked",true);
+		}
+		
+		$("#city").val($tableItems.eq(6).html());
+		$("#datepicker").val($tableItems.eq(7).html());
+		$("#idNumber").val($tableItems.eq(8).html());
+
+		$form.slideUp();
+		$table.slideDown();
+	});
+});
